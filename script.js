2dae5f640fa9884748fe5bab99a9b18ae784bad2
Milos Dimitrijevic <dimitrijevic.milos40@gmail.com>
2024-04-21 11:54:18 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("keydown" , (event) => {
+  let code = event.keyCode;
+
+  let audio = document.querySelector(`audio[data-key="${code}"]`);
+  audio.currentTime = 0;  
+  audio.play();
+})
